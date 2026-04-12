6d3ccf6e084f895ffe5454336d7293a743c00c92
user <user@example.com>
2026-04-12 09:21:14 +0200

    Edited foxhole plugin

--- coreplugins/posm-gcpi-foxhole/plugin.py
+++ coreplugins/posm-gcpi-foxhole/plugin.py
@@ -1,28 +1,20 @@
 from app.plugins import PluginBase, Menu, MountPoint
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext as _
-
-
+from django.contrib.auth.decorators import login_required
 
 class Plugin(PluginBase):
+
     def main_menu(self):
-        return [Menu(_("Hello World"), self.public_url(""), "fa fa-cog fa-fw")]
+        return [Menu(_("GCP Interface"), self.public_url(""), "fa fa-map-marker-alt fa-fw")]
 
     def app_mount_points(self):
         @login_required
-        def hello_view(request):
-            return render(request, self.template_path("hello.html"), {'message': "Hello!"})
+        def gcpi(request):
+            return render(request, self.template_path("app.html"), {'title': 'GCP Editor for Foxhole'})
 
         return [
-            MountPoint('$', hello_view),
-            # more mount points here ...
+            MountPoint('$', gcpi)
         ]
-    
-    def include_js_files(self):
-        return ['main.js']
-    
-    def build_jsx_components(self):
-        return ['app.jsx']
 
-    # see also plugin_base.py for more methods
+
